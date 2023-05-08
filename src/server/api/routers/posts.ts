9fea0f87d50e15d2887a4030fd1b4936ec059cff
Weiyi Lee <weiyilee@Weiyide-iMac-2.router00195f.com>
2023-05-08 16:10:30 +0800

    Styled up and used next image

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,3 +1,5 @@
+import { User, clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -5,8 +7,41 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 // A procedure is a method generate function that your client calls
 // A publicProcedure is a procedure that anyone can call
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const allUsers = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorID),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = allUsers.find((user) => user.id === post.authorID);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+
+      return {
+        post,
+        author,
+      };
+    });
   }),
 });
