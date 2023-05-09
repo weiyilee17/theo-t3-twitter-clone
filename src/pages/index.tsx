b45b66c3492949f8e0f5db527eb68e5b59d8b8f0
Weiyi Lee <weiyilee@Weiyide-iMac-2.router00195f.com>
2023-05-09 18:11:52 +0800

    Made posts work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,11 +9,24 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage } from "~/components.tsx/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      // void makes typescript not have to await a promise
+      void ctx.posts.getAll.invalidate();
+    },
+  });
 
   if (!user) {
     return null;
@@ -32,7 +45,11 @@ const CreatePostWizard = () => {
         className="grow bg-transparent outline-none"
         type="text"
         placeholder="Type some emojis!"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
@@ -76,7 +93,7 @@ const Feed = () => {
 
   return (
     <div className="flex flex-col">
-      {[...data, ...data]?.map((fullPost) => (
+      {data?.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
